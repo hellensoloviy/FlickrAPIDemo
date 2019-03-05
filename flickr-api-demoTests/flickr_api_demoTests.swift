753eb8d8f327ec667f8cf7038ba2e9b00983ed1d
Hellen S <hellensoloviy@gmail.com>
2019-03-05 15:54:45 +0200

    Initial Commit

---
+++ flickr-api-demoTests/flickr_api_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  flickr_api_demoTests.swift
+//  flickr-api-demoTests
+//
+//  Created by Hellen Soloviy on 3/5/19.
+//  Copyright © 2019 HellySolovii. All rights reserved.
+//
+
+import XCTest
+@testable import flickr_api_demo
+
+class flickr_api_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
