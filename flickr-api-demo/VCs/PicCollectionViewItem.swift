01c5ceb762f0876a759c9e0780d853baf5e67710
Hellen S <hellensoloviy@gmail.com>
2019-03-05 16:05:52 +0200

    add controller, API helpers, basic UI

---
+++ flickr-api-demo/VCs/PicCollectionViewItem.swift
@@ -0,0 +1,18 @@
+//
+//  PicCollectionViewItem.swift
+//  flickr-api-demo
+//
+//  Created by Hellen Soloviy on 3/4/19.
+//  Copyright © 2019 HellySolovii. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PicCollectionViewItem: UICollectionViewCell {
+    static let identifier = "PicReusableCell"
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
